48eccdfae79d5fdba8a0e084ad2dff1c0daf97c5
kurbster <kkuznia28@gmail.com>
2022-11-07 13:28:40 -0700

    updated open ai call.

--- src/gen_sql.py
+++ src/gen_sql.py
@@ -4,7 +4,7 @@ import time
 import logging
 
 from typing import Dict
-from datetime import datetime
+from collections import deque
 from pathlib import Path
 PARENT_DIR = Path(__file__, '../..').resolve()
 sys.path.append(str(PARENT_DIR))
@@ -21,28 +21,48 @@ from src.config import (
 )
 
 from lib.create_few_shot_prompt import generate_prompts
+from lib.write_output import write_output
 
 logger = logging.getLogger('apiLogger')
 
 def query_model(api_cfg: APIConfig, gen_cfg: GenerationConfig, prompts: Dict[str, str]) -> Dict[str, str]:
+    output_queue = deque()
     api_cfg = OmegaConf.to_container(api_cfg)
+    def call_model(prompt, itr, db_name):
+        # response = openai.Completion.create(
+            # prompt=prompt,
+            # **api_cfg
+        # )
+        response = {"choices": [{"text": 
+        " Give me the formuoli\nSQL: select ravioli from formuoli"
+        }]}
+        result = parse_response(response, gen_cfg.query_prefix)
+        result['input'] = prompt
+        # Append the output from the model to the prompt
+        prompt += f"{result['output']}\n\n{gen_cfg.suffix}"
+        # Save the gpt response json, the input/output/query/question
+        # To the experiment directory so we keep track of everything
+        output_queue.append({(db_name, 'response', itr): response})
+        output_queue.append({(db_name, 'result', itr): result})
+        # Save the question query pair directly to the data dir
+        output_queue.append({(db_name, 'pair', itr): 
+            {'question': result['question'], 'query': result['question']}
+        })
+        return prompt
+
     for db_name, prompt in prompts.items():
         for i in range(gen_cfg.n_generations_per_database):
-            logger.info(f'I am prompt in {i} iteration\n{prompt}')
+            logger.info(f'I am prompt for {db_name} in {i} iteration\n{prompt}')
             try:
-                # response = openai.Completion.create(
-                    # prompt=prompt,
-                    # **api_cfg
-                # )
-                response = {"choices": [{"text": 
-                " Give me the formuoli\nSQL: select ravioli from formuoli"
-                }]}
-                result = parse_response(response, gen_cfg.query_prefix)
-                result['input'] = prompt
-                prompt += f"{result['output']}\n\n{gen_cfg.suffix}"
-            except RateLimitError as e:
-                start = datetime.now()
+                prompt = call_model(prompt, i, db_name)
+            except RateLimitError:
                 logger.error('We have hit our rate limit. Writing output then sleeping.')
+                seconds_spent = write_output(output_queue)
+                logger.debug(f'Spent: {seconds_spent} writing output')
+                time.sleep(61 - seconds_spent)
+                prompt = call_model(prompt)
+    # Write any remaining output
+    write_output(output_queue)
 
 def parse_response(response: Dict[str, str], sql_prefix: str) -> Dict[str, str]:
     output = response['choices'][0]['text']
